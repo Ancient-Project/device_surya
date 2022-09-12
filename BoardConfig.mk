905e021ad5a86331fecc4315bd0e3e6dfbf8aa1b
rezaadi0105 <rezaadipangestu5@gmail.com>
2022-09-12 12:57:00 +0700

    surya: tmp: permissive
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,6 +118,7 @@ BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # LMKD
 TARGET_LMKD_STATS_LOG := true
@@ -196,12 +197,12 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 TARGET_SCREEN_DENSITY := 400
 
 # Sepolicy
-TARGET_SEPOLICY_DIR := msmsteppe
-include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
+#TARGET_SEPOLICY_DIR := msmsteppe
+#include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 
-BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
-BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+#BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
+#BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+#SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Vendor security patch level
 VENDOR_SECURITY_PATCH := 2022-06-05
